fd6b0d23cdd6d48f0ab992cc53b514cda7328bbd
Naga-Rajasekhar <rajasekhar9860@gmail.com>
2023-01-19 09:37:51 +0530

    practised ejs basics

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+
+//View engine Middleware(to render the data)
+app.set("view engine", "ejs");
+
+//middleware(to use json inside express, we need to use express)
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+//My home Route
+
+app.get("/", (req, res) => {
+  res.send("Hello Express");
+});
+
+//My get Route
+
+app.get("/myget", (req, res) => {
+  console.log(req.query);
+  res.send(req.query);
+});
+
+//My post route
+
+app.post("/mypost", (req, res) => {
+  console.log(req.body);
+  console.log(req.files);
+});
+
+app.get("/getform", (req, res) => {
+  res.render("getform");
+});
+
+//rendering page for post
+
+app.get("/postform", (req, res) => {
+  res.render("postform");
+});
+
+app.listen(4000, () => {
+  console.log(`Server is running at 4000`);
+});
